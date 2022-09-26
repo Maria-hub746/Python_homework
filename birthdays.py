f3220cd0d68940fee3579831f0f36ac233ca5495
Maria-hub746 <113303447+Maria-hub746@users.noreply.github.com>
2022-09-26 19:07:30 +0300

    Update birthdays.py

--- birthdays.py
+++ birthdays.py
@@ -1,41 +1,62 @@
-from datetime import datetime
-import calendar
+from datetime import datetime, timedelta
 
 
-def get_birthdays_per_week(users):
-    result = {'Monday': [],
-        'Tuesday':[],
-        'Wednesday' : [],
-        'Thursday' : [],
-        'Friday' : []
-    }
-
-    today_now = datetime.now()
-    for user in users:
-        name = user['name']
-        birthday = user['birthday']
-        difference = birthday - today_now
-        difference = difference.days
-        if difference < 7:
-            week = birthday.strftime('%A')
-            if week == 'Saturday' or week == 'Sunday':
-                week = 'Monday'
-                result[week].append(name)
-                
-            else:
-                result[week].append(name)
-                
-    return result
-                
-
-                    
-
-
-            
-
-user = [
+users_list = [
 {'name': 'Alex', 'birthday': datetime(year=2022, month=10, day=1)},
-{'name': 'Lily', 'birthday': datetime(year=2022, month=9, day=25)}
+{'name': 'Lily', 'birthday': datetime(year=2022, month=9, day=25)},
+{'name': 'Stasy', 'birthday': datetime(year=2022, month=9, day=27)},
+{'name': 'Mia', 'birthday': datetime(year=2022, month=9, day=29)},
+{'name': 'Bella', 'birthday': datetime(year=2022, month=9, day=26)},
+{'name': 'Violetta', 'birthday': datetime(year=2022, month=9, day=30)},
+{'name': 'Emma', 'birthday': datetime(year=2022, month=10, day=5)},
+{'name': 'Susan', 'birthday': datetime(year=2022, month=10, day=3)},
+{'name': 'Anna', 'birthday': datetime(year=2022, month=10, day=2)},
+{'name': 'Regena', 'birthday': datetime(year=2022, month=10, day=4)}
 ]
 
-print(get_birthdays_per_week(user))
+def get_birthdays_per_week(users: list):
+    result_list = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": []
+    }
+
+    now_day = datetime.now()
+    days_interval = define_days_interval(now_day)
+    new_time_line = now_day + days_interval
+
+    for user in users:
+        name = user['name']
+        birthday = datetime(
+            year=now_day.year,
+            month=user.get('birthday').month,
+            day=user.get('birthday').day
+        )
+        if now_day < birthday <= new_time_line:
+            weekday = birthday.strftime("%A")
+            if weekday in ['Saturday', 'Sunday']:
+                weekday = 'Monday'
+            result_list[weekday].append(name)
+    print_users_list(result_list)
+
+
+def define_days_interval(now_day: datetime) -> timedelta:
+    if now_day.weekday() == 5:
+        days_interval = timedelta(days=6)
+    elif now_day.weekday() == 6:
+        days_interval = timedelta(days=5)
+    else:
+        days_interval = timedelta(days=7)
+
+    return days_interval
+
+def print_users_list(users_list: dict):
+    for key, value in users_list.items():
+        if value:
+            print(f"{key}: {', '.join(value)}")
+
+if __name__ == '__main__':
+    get_birthdays_per_week(users_list)
+
